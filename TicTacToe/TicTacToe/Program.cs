20ba252afa246847a527d04feb26ef68e57b28b4
Judah Hunger <judah.hunger@gmail.com>
2018-06-11 11:40:57 -0700

    intial push

---
+++ TicTacToe/TicTacToe/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.IO;
+
+namespace TicTacToe
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+       
+        static void CreateGameFile(string path, string[] ArraytoWrite)
+        {
+            
+            if (!File.Exists(path))
+            {
+                using (StreamWriter sw = new StreamWriter(path))
+                {
+                    try
+                    {
+
+                    }
+                    catch (Exception)
+                    {
+
+                        throw;
+                    }
+                }
+            }
+        }
+    }
+}
