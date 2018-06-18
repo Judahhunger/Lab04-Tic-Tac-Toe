a87ca3a16bdfb060e2e65d99c4ed8f2eb8906cc8
Judah Hunger <judah.hunger@gmail.com>
2018-06-18 13:39:19 -0700

    app working just need to add tests and fix bugs

--- TicTacToe/TicTacToe/Classes/Players.cs
+++ TicTacToe/TicTacToe/Classes/Players.cs
@@ -4,7 +4,30 @@ using System.Text;
 
 namespace TicTacToe.Classes
 {
-    class Players
+    public class Players
     {
+        /// <summary>
+        /// Stores players name
+        /// </summary>
+        public string PlayerName { get; set; }
+
+        /// <summary>
+        /// Stores players game marker
+        /// </summary>
+        public string PlayerGamePiece { get; set; }
+
+
+        /// <summary>
+        /// stores the positions they players have chossen.
+        /// </summary>
+        public string[] PlayerPositions { get; set; }
+
+        
+        public Players(string name, string marker)
+        {
+            PlayerName = name;
+            PlayerGamePiece = marker;
+        }
+
     }
 }
--- TicTacToe/TicTacToe/Program.cs
+++ TicTacToe/TicTacToe/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.IO;
+using TicTacToe.Classes;
 
 namespace TicTacToe
 {
@@ -7,28 +8,122 @@ namespace TicTacToe
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Play a game of Tic Tack Toe");
+            PlayTicTacToe();
         }
 
-       
-        static void CreateGameFile(string path, string[] ArraytoWrite)
+        /// <summary>
+        /// Makes player one with name and marker
+        /// </summary>
+        /// <returns>Player one</returns>
+        public static Players GetPlayerOne()
         {
-            
-            if (!File.Exists(path))
+            Console.WriteLine("Player one please enter your name");
+            string playerOneName = Console.ReadLine();
+            Console.WriteLine($"{playerOneName} You are X's");
+            Console.ReadLine();
+            Console.Clear();
+
+            Players playerOne = new Players(playerOneName, "X");
+            playerOne.PlayerPositions = new string[5];
+
+            return playerOne;
+        }
+
+        /// <summary>
+        /// Makes player two with name and marker
+        /// </summary>
+        /// <returns>Player two</returns>
+        public static Players GetPlayerTwo()
+        {
+            Console.WriteLine("Player two please enter your name");
+            string playerTwoName = Console.ReadLine();
+            Console.WriteLine($"{playerTwoName} You are O's");
+            Console.ReadLine();
+            Console.Clear();
+
+            Players playerTwo = new Players(playerTwoName, "O");
+            playerTwo.PlayerPositions = new string[4];
+            return playerTwo;
+        }
+
+        /// <summary>
+        /// Gets players, board and starts game.
+        /// </summary>
+        public static void PlayTicTacToe()
+        {
+            Players readyPlayerOne = GetPlayerOne();
+            Players readyPlayerTwo = GetPlayerTwo();
+            GameBoard mainGameBoard = new GameBoard();
+
+            string[,] newGameBoard = mainGameBoard.DefualtBoard;
+
+            Game game = new Game();
+            game.PlayerOne = readyPlayerOne;
+            game.PlayerTwo = readyPlayerTwo;
+
+            // winner = false;
+            int counter = 0; // game counter
+            int playOneCount = 0;//makes index in array for player one
+            int playTwoCount = 0;//makes index in array for player two
+            while (game.Winner == false && counter < 9)
             {
-                using (StreamWriter sw = new StreamWriter(path))
+                mainGameBoard.ShowBoard(newGameBoard);
+               
+                if (counter % 2 == 0)
                 {
-                    try
-                    {
-
-                    }
-                    catch (Exception)
-                    {
-
-                        throw;
-                    }
+                    
+                    Console.WriteLine($"{readyPlayerOne.PlayerName} please choose a position");
+                    string playerOnePositionSelected = Console.ReadLine();
+                    readyPlayerOne.PlayerPositions[playOneCount] = playerOnePositionSelected;
+                    mainGameBoard.UpdateGamePositions(readyPlayerOne, readyPlayerTwo, newGameBoard);
+                    playOneCount++;
+                    counter++;
+  
                 }
+                else
+                { 
+                    Console.WriteLine($"{readyPlayerTwo.PlayerName} please choose a position");
+                    string playerTwoPositionSelected = Console.ReadLine();
+                    readyPlayerTwo.PlayerPositions[playTwoCount] = playerTwoPositionSelected;
+                    mainGameBoard.UpdateGamePositions(readyPlayerOne, readyPlayerTwo, newGameBoard);
+                    playTwoCount++;
+                    counter++;  
+                }
+
+                game.Winner = game.CheckForWinner(readyPlayerOne, readyPlayerTwo);
+               
+            }
+            if (game.Winner == true)
+            {
+                PlayAgain();
+            }
+            Console.WriteLine("the match was a draw");
+            Console.ReadLine();
+            PlayAgain();
+        }
+
+        /// <summary>
+        /// asks if players want to start again and if they do starts new game if not exits app.
+        /// </summary>
+        public static void PlayAgain()
+        {
+            Console.WriteLine();
+            Console.WriteLine("Would you like to play again?");
+            Console.WriteLine("1) play again");
+            Console.WriteLine("2) to exit app");
+            string playAgainOrNot = Console.ReadLine();
+
+            switch (playAgainOrNot)
+            {
+                case "1":
+                    PlayTicTacToe();
+                    break;
+                default:
+                    Environment.Exit(0);
+                    break;
             }
         }
+       
     }
 }
